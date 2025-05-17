eceab2c6bba37c458151236f59090ec181fb2c0b
azucar <jim.duindam55@gmail.com>
2025-05-17 16:54:00 +0200

    stam and health bar rudimentary

---
+++ progress_bar.gd
@@ -0,0 +1,15 @@
+extends ProgressBar
+
+@export var max_health: int = 100
+var current_health: int = max_health
+
+func _ready():
+	update_bar()
+
+func update_bar():
+	value = current_health
+
+func take_damage(amount: int):
+	current_health = max(current_health - amount, 0)
+	update_bar()
+	
---
+++ stam_bar.gd
@@ -0,0 +1,33 @@
+extends ProgressBar
+
+@export var max_stam: int = 120
+var current_stam: float = max_stam
+
+@export var regen_delay: float = 1.0  # Time to wait before regenerating
+@export var regen_rate: float = 10.0 # Stamina per second
+
+var time_since_use: float = 0.0
+var used_stamina: bool = false
+
+func _ready():
+	update_bar()
+
+func _process(delta):
+	if current_stam < max_stam:
+		if used_stamina:
+			time_since_use += delta
+			if time_since_use >= regen_delay:
+				used_stamina = false  # Done waiting, start regenerating
+		else:
+			current_stam += regen_rate * delta
+			current_stam = min(current_stam, max_stam)
+			update_bar()
+
+func stam_used(amount: int):
+	current_stam = max(current_stam - amount, 0)
+	used_stamina = true
+	time_since_use = 0.0  # Reset regen delay timer
+	update_bar()
+
+func update_bar():
+	value = current_stam
