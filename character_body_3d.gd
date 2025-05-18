e5a13f70010f58b051143ef290156604dcfe5a01
razer0003 <razorthepanda@gmail.com>
2025-05-18 19:57:30 -0400

    Reapply "massive changes to player movement, animations"
    This reverts commit cdedf87da85ce60ca2a5032d9982d510b18a0995.

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,10 +1,14 @@
 extends CharacterBody3D
 
-const SPEED = 5.8
+const SPEED = 2.5
 const JUMP_VELOCITY = 5
 const SENSITIVITY = 0.002
-const DODGE_SPEED = 13
-const DODGE_TIME = 0.27
+const DODGE_SPEED = 13.0
+const DODGE_TIME = 0.75 # Total dodge duration
+const DODGE_BURST_TIME = 0.2 # Time for initial burst
+const DODGE_BURST_SPEED = 13.0 # Fast burst speed
+const DODGE_RECOVERY_SPEED = 2.0 # Slow recovery speed
+const DODGE_ANIMATION_LENGTH = 2.6 # Length of your dodge anim in seconds
 const DODGE_COOLDOWN = 0.5
 const AIR_CONTROL = 0.03
 const SPRINT_MULTIPLIER = 1.5
@@ -26,6 +30,8 @@ const ROTATION_LERP_SPEED = 10.0
 @onready var body = $body
 @onready var playermodel = $mesh/playermodel
 
+@onready var animtree = $AnimationTree
+
 var dodge_timer := 0.0
 var dodge_cooldown := 0.0
 var dodge_direction := Vector3.ZERO
@@ -38,12 +44,24 @@ var current_dodge_speed := DODGE_SPEED
 var current_dodge_time := DODGE_TIME
 var far_dodge := false
 
-
+# Animation blend variables
+var walk_blend := 0.0
+const WALK_BLEND_SPEED = 8.0
+var sprint_blend := 0.0
+const SPRINT_BLEND_SPEED = 8.0
+var dodge_blend := 0.0
+const DODGE_BLEND_SPEED = 20.0 # Dodge should snap in/out quickly
 
 func _ready():
+	$AnimationPlayer.get_animation("Walk").loop = true
+	$AnimationPlayer.get_animation("Idle").loop = true
+	$AnimationPlayer.get_animation("Sprint").loop = true
+	$AnimationPlayer.get_animation("Dodge").loop = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	camera.fov = BASE_FOV
 
+	if animtree:
+		animtree.active = true
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
@@ -70,6 +88,7 @@ func _physics_process(delta):
 	elif dodge_cooldown > 0.0:
 		dodge_cooldown -= delta
 
+	# Dodge input: use "dodge" or "move_jump" depending on your input map
 	if Input.is_action_just_pressed("dodge") and is_on_floor() and not is_dodging and dodge_cooldown <= 0.0 and stam_bar.current_stam >= 10:
 		stam_bar.stam_used(10)
 		
@@ -95,45 +114,78 @@ func _physics_process(delta):
 		target_rotation_y = atan2(dodge_direction.x, dodge_direction.z)
 
 	if is_dodging:
-		velocity.x = dodge_direction.x * current_dodge_speed
-		velocity.z = dodge_direction.z * current_dodge_speed
+		# Two-phase dodge: fast burst, then slow recovery
+		var elapsed = current_dodge_time - dodge_timer
+		var burst_phase = elapsed < DODGE_BURST_TIME
+		var dodge_speed = DODGE_BURST_SPEED if burst_phase else DODGE_RECOVERY_SPEED
+		velocity.x = dodge_direction.x * dodge_speed
+		velocity.z = dodge_direction.z * dodge_speed
 		velocity.y = 0
-	elif not is_on_floor():
-		if not in_air:
-			in_air = true
-			locked_air_velocity.x = velocity.x
-			locked_air_velocity.z = velocity.z
-		velocity.y -= gravity * delta
 
-		var input_dir = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
-		var forward_dir = -camera.global_transform.basis.z
-		var right_dir = camera.global_transform.basis.x
-		var air_direction = (forward_dir * input_dir.y + right_dir * input_dir.x).normalized()
-
-		if air_direction != Vector3.ZERO:
-			velocity.x = lerp(velocity.x, air_direction.x * current_speed, AIR_CONTROL)
-			velocity.z = lerp(velocity.z, air_direction.z * current_speed, AIR_CONTROL)
-			target_rotation_y = atan2(velocity.x, velocity.z)
+		# Sync dodge animation speed to match dodge time
+		var playback_speed = DODGE_ANIMATION_LENGTH / current_dodge_time
+		$AnimationPlayer.playback_speed = playback_speed
+		if animtree:
+			animtree["parameters/Dodge/scale"] = playback_speed
 	else:
-		in_air = false
-		if Input.is_action_just_pressed("move_jump") and stam_bar.current_stam >= 10:
-			stam_bar.stam_used(10)
-			velocity.y = JUMP_VELOCITY
-			locked_air_velocity.x = velocity.x
-			locked_air_velocity.z = velocity.z
-		var input_dir = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
-		var forward_dir = -camera.global_transform.basis.z
-		var right_dir = camera.global_transform.basis.x
-		var direction = (forward_dir * input_dir.y + right_dir * input_dir.x).normalized()
+		# Normal movement when not dodging
+		$AnimationPlayer.playback_speed = 1.0
+		if animtree:
+			animtree["parameters/Dodge/scale"] = 1.0
 
-		if direction != Vector3.ZERO:
-			velocity.x = direction.x * current_speed
-			velocity.z = direction.z * current_speed
-			target_rotation_y = atan2(direction.x, direction.z)
+		if not is_on_floor():
+			if not in_air:
+				in_air = true
+				locked_air_velocity.x = velocity.x
+				locked_air_velocity.z = velocity.z
+			velocity.y -= gravity * delta
+
+			var input_dir = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
+			var forward_dir = -camera.global_transform.basis.z
+			var right_dir = camera.global_transform.basis.x
+			var air_direction = (forward_dir * input_dir.y + right_dir * input_dir.x).normalized()
+
+			if air_direction != Vector3.ZERO:
+				velocity.x = lerp(velocity.x, air_direction.x * current_speed, AIR_CONTROL)
+				velocity.z = lerp(velocity.z, air_direction.z * current_speed, AIR_CONTROL)
+				target_rotation_y = atan2(velocity.x, velocity.z)
 		else:
-			velocity.x = 0
-			velocity.z = 0
+			in_air = false
+			if Input.is_action_just_pressed("move_jump") and stam_bar.current_stam >= 10:
+				stam_bar.stam_used(10)
+				velocity.y = JUMP_VELOCITY
+				locked_air_velocity.x = velocity.x
+				locked_air_velocity.z = velocity.z
+			var input_dir = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
+			var forward_dir = -camera.global_transform.basis.z
+			var right_dir = camera.global_transform.basis.x
+			var direction = (forward_dir * input_dir.y + right_dir * input_dir.x).normalized()
+
+			if direction != Vector3.ZERO:
+				velocity.x = direction.x * current_speed
+				velocity.z = direction.z * current_speed
+				target_rotation_y = atan2(direction.x, direction.z)
+			else:
+				velocity.x = 0
+				velocity.z = 0
 			
 	playermodel.rotation.y = lerp_angle(playermodel.rotation.y, target_rotation_y, ROTATION_LERP_SPEED * delta)
-	
+
+	# --- AnimationTree blend logic (Godot 4.x) ---
+	var input_dir = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
+	var is_walking = input_dir.length() > 0.01 and is_on_floor() and not is_dodging
+	var target_walk_blend = 1.0 if is_walking else 0.0
+	walk_blend = lerp(walk_blend, target_walk_blend, WALK_BLEND_SPEED * delta)
+
+	var target_sprint_blend = 1.0 if sprinting else 0.0
+	sprint_blend = lerp(sprint_blend, target_sprint_blend, SPRINT_BLEND_SPEED * delta)
+
+	var target_dodge_blend = 1.0 if is_dodging else 0.0
+	dodge_blend = lerp(dodge_blend, target_dodge_blend, DODGE_BLEND_SPEED * delta)
+
+	if animtree:
+		animtree["parameters/Walk/blend_amount"] = walk_blend
+		animtree["parameters/Sprint/blend_amount"] = sprint_blend
+		animtree["parameters/Dodge/blend_amount"] = dodge_blend
+
 	move_and_slide()
